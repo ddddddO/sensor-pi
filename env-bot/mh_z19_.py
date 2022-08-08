ce4ee275ab8836324898ec4ae0accb0bcb528b8d
ddddddO <lbfdeatq@gmail.com>
2022-08-08 16:50:08 +0900

    add mh_z19 process

---
+++ env-bot/mh_z19_.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import mh_z19
+import os
+import datetime
+import sqlite3
+from pypika import Query, Table, Order
+
+class Repository:
+	def __init__(self, dsn) -> None:
+		self.conn = sqlite3.connect(dsn)
+
+	def fetch(self) -> (float, float, float):
+		mh_z19 = Table('mh_z19')
+		q = Query.from_(mh_z19)\
+			.select(mh_z19.co2)\
+			.orderby(mh_z19.date, order=Order.desc)\
+			.limit(1)
+
+		cur = self.conn.cursor()
+		cur.execute(str(q))
+		row = cur.fetchone()
+		c = row[0]
+		return c
+
+	def store(self, co2):
+		date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+		mh_z19 = Table('mh_z19')
+		q = Query.into(mh_z19)\
+			.columns(mh_z19.date, mh_z19.co2)\
+			.insert(date, co2)
+
+		cur = self.conn.cursor()
+		cur.execute(str(q))
+		self.conn.commit()
+
+	def close(self):
+		self.conn.close()
+
+
+ret = mh_z19.read()
+co2 = ret['co2'] # TODO: KeyError handling. retryする？
+
+dsn = os.getenv('DSN')
+repo = Repository(dsn)
+
+repo.store(co2)
