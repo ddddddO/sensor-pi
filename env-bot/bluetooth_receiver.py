79340075af2e3501d75cc7204b177459c5f26943
ddddddO <lbfdeatq@gmail.com>
2022-10-01 16:51:58 +0900

    add Bluetooth receiver

---
+++ env-bot/bluetooth_receiver.py
@@ -0,0 +1,27 @@
+# ref: https://monomonotech.jp/kurage/raspberrypi/daiso_btshutter.html
+from asyncio import subprocess
+import evdev
+# import subprocess
+import time
+
+while True:
+    try:
+        # ls /dev/input でevent番号要確認
+        device = evdev.InputDevice('/dev/input/event0')
+        print(device)
+
+        for event in device.read_loop():
+            if event.type == evdev.ecodes.EV_KEY:
+                if event.value == 1: # 0:KEYUP, 1:KEYDOWN
+                    print(event.code)
+
+                    if event.code == evdev.ecodes.KEY_VOLUMEUP:
+                        print('Received!')
+                        subprocess.Popen(['/home/pi/go/bin/dagu', 'start', '/home/pi/github.com/ddddddO/sensor-pi/env-bot/dag.yaml'])
+
+                    # if event.code == evdev.ecodes.KEY_ENTER:
+                    #    subprocess.Popen(['amixer', 'sset', 'PCM', '10%-', '-M'])
+                    #    print('Down!')
+    except:
+        print('Retry...')
+        time.sleep(1)
